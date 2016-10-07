c3752f88997ba1d8508a38376f266c24f99f5f09
annibessie <annibessie.kitt@gmail.com>
2016-10-07 22:21:52 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by Anni-Bessie on 7.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
